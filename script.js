468f840c5577ab4facba8019e576ae7910e18272
Ahmad-Mohammed-Bakadam <98517205+Ahmad-Mohammed-Bakadam@users.noreply.github.com>
2023-10-22 11:37:52 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const initLikes = 220;
+const initDisLikes = 34;
+
+let likesCount = initLikes
+let dislikesCount = initDisLikes
+
+let LikeBtn = document.getElementById("likeBtn");
+let disLikeBtn = document.getElementById("dislikeBtn");
+
+likeBtn.innerText = "👍" + likesCount
+dislikeBtn.innerText = "👎" + dislikesCount
+
+
+function doLike() {
+    likesCount++;
+    likeBtn.innerText = "👍 " + likesCount
+    disableButtons();
+    setCookie();
+}
+
+function doDislike() {
+    dislikesCount++;
+    dislikeBtn.innerText = "👎 " + dislikesCountlikesCount
+    disableButtons();
+    setCookie();
+}
+
+function disableButtons() {
+    likeBtn.disabled = true;
+    disLikeBtn.disabled = true;
+}
+
+function setCookie() {
+    document.cookie = "voted=true; SameSite=Strict; Max-Age=" + 60
+}
+
+
+window.onload = function () {
+    if (document.cookie && document.cookie.indexOf("voted") > -1) {
+        disableButtons();
+    }
+}
+
+
+
